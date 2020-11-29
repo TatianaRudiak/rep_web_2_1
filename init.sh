3bf026d92033df94ece1f233f7ec93d2c620785e
Tatiana <tanchaz@ya.ru>
2020-11-29 20:00:09 +0300

    daaaaaaaa

--- init.sh
+++ init.sh
@@ -1,9 +1,3 @@
-sudo pip3 install virtualenv
-sudo pip3 install pathlib2
-virtualenv -p python3 venv
-source venv/bin/activate
-pip3 install django
-
 sudo rm -rf /etc/nginx/sites-enabled/default
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
