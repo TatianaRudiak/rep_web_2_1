03ac254663103ce8b8a9d269ef4c63e5340ad705
Tatiana <tanchaz@ya.ru>
2020-12-01 15:17:17 +0300

    lesson 2_3 final

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,28 +1,29 @@
 from __future__ import unicode_literals
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=250)
-    text = models.TextField()
-    added_at = models.DateTimeField(blank = True, auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name='question'
-    )
-    likes = models.ManyToMany(User, related_name='question_like_user')   # список пользователей, поставивших "лайк"
-
-    class Meta:
-        ordering = ['-added_at']
-
-    class QuestionManager(models.Manager): 
-        objects = QuestionManager()   
+class QuestionManager(models.Manager):    
         def new(self,count=1):
             return self.objects.all()[:count]
         
         def popular(self):
             return self.objects.order_by('-rating')
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name='question'
+    )
+    likes = models.ManyToManyField(User, related_name='question_like_user')  
+    
+    class Meta:
+        ordering = ['-added_at']
+
 
 class Answer(models.Model):
     text = models.TextField()
@@ -31,5 +32,5 @@ class Answer(models.Model):
         Question, on_delete=models.CASCADE, related_name='answer'
     )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL
+        User, null=True, on_delete=models.SET_NULL
     )
